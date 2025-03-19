85048cb1a4dfe97b886eb922ab5c67d80d1ae9cc
prince mumo <princeinkunzi@gmail.com>
2025-03-19 12:24:36 +0300

    updating code

--- src/components/Groups/GroupTable.jsx
+++ src/components/Groups/GroupTable.jsx
@@ -64,7 +64,7 @@ const GroupTable = () => {
 
   const handleManageRoles = (group) => {
     setManagingRoles(group);
-    const { createdBy, createdDate, ...roles } = group;
+    const {createdDate, ...roles } = group;
     setRoleFormData({ ...roles });
     setMode('roles');
   };
